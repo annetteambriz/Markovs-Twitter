918912ef1af27f27190bb1d75ab6947ca72c8432
Annette Ambriz <annette.ambriz@creditkarma.com>
2018-09-04 19:59:07 -0700

    Added .gitignore

--- secrets.sh
+++ secrets.sh
@@ -1,8 +1,2 @@
-# Secrets for your Twitter app
-#
-# This file should never go into github!
-
-export TWITTER_CONSUMER_KEY="your-consumer-key"
-export TWITTER_CONSUMER_SECRET="your-consumer-secret"
-export TWITTER_ACCESS_TOKEN_KEY="your-access-token-key"
-export TWITTER_ACCESS_TOKEN_SECRET="your-access-token-secret"
+.pyc*
+secrets.sh
